4f04d10a5888644de44b5541255390e15ed7a2e7
Rado <radoslav.yankov96@gmail.com>
2023-02-15 17:10:19 +0100

    first commit

---
+++ model.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+from tensorfrlow import keras
+from tensorflow.keras import layers
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+print("hello")
