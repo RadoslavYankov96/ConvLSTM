366d50e6f5cc56ebc11aac2959053e3604beeeed
Rado <radoslav.yankov96@gmail.com>
2023-02-16 12:19:19 +0100

    update the sequence predictor class

--- model.py
+++ model.py
@@ -1,38 +1,63 @@
 import tensorflow as tf
 from tensorflow import keras
 from keras import layers
-from keras.layers import ConvLSTM2D, BatchNormalization, MaxPooling3D, TimeDistributed, Dense
+from keras.layers import ConvLSTM2D, BatchNormalization, MaxPooling3D, Conv3D
 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
 
-class SequencePredictor:
-    def __init__(self, kernel, num_layers, filters, input_shape, frames, pool):
+class SequencePredictor(keras.Sequential):
+
+    """
+    General Class for the next sequence predictor
+    Args:
+    """
+
+    def __init__(self, kernel, num_layers, filters, frames, input_format, pool):
+        super().__init__()
         self.kernel = kernel
         self.num_layers = num_layers
         self.filters = filters
-        self.input_shape = input_shape
         self.frames = frames
+        self.input_format = input_format
         self.pool = pool
 
-    def build_model(self):
-        model = keras.Sequential()
+    def build_encoder(self):
+
+        """
+        Building the encoder part of the model based on a ConvLSTM architecture for next sequence prediction.
+        This function initializes the model
+        """
+
         for i in range(self.num_layers):
-            model.add(ConvLSTM2D(filters=self.filters[i], kernel_size=self.kernel, padding='same', activation='tanh',
-                                 recurrent_activation='hard_sigmoid', input_shape=self.input_shape[i],
-                                 return_sequences=True))
-            model.add(keras.layers.ReLU())
-            model.add(BatchNormalization())
-            model.add(MaxPooling3D(pool_size=self.pool, padding='same'))
+            self.add(ConvLSTM2D(filters=self.filters[i], kernel_size=self.kernel, padding='same', activation='tanh',
+                                recurrent_activation='hard_sigmoid', input_shape=self.input_format,
+                                return_sequences=True))
+            self.add(keras.layers.ReLU())
+            self.add(BatchNormalization())
+            self.add(MaxPooling3D(pool_size=self.pool, padding='same'))
 
-        return model
+    def build_decoder(self):
+        for i in range(self.num_layers-1, -1, -1):
+            self.add(layers.UpSampling3D(size=self.pool))
+            self.add(ConvLSTM2D(filters=self.filters[i], kernel_size=self.kernel, padding='same', activation='tanh',
+                                recurrent_activation='hard_sigmoid', return_sequences=True))
+            self.add(layers.ReLU())
+            self.add(BatchNormalization())
+        self.add(ConvLSTM2D(filters=2, kernel_size=self.kernel, padding='same', activation='tanh',
+                            recurrent_activation='hard_sigmoid', return_sequences=True))
+
+    def build_model(self):
+        self.build_encoder()
+        self.build_decoder()
 
 
-first_model = SequencePredictor(3, 3, [16, 32, 64], [(2, 600, 400, 2), (2, 400, 200, 16),
-                                                             (2, 200, 100, 32)],
-                                2, (3, 3, 1))
-first_model = first_model.build_model()
+first_model = SequencePredictor(5, 3, [16, 32, 32], 2, [2, 600, 400, 2], (1, 2, 2))
+first_model.build_model()
 first_model.summary()
 
+'''this is just an example for trying out
+the idea is to create dictionaries with parameters 
+'''
