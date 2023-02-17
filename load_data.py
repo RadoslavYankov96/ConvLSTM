5262211b0c355f440d5aadbefab7c59dc9819284
Rado <radoslav.yankov96@gmail.com>
2023-02-17 18:13:29 +0100

    improving the data structure

---
+++ load_data.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+import tensorflow_datasets as tfds
+
+import os
+import operator
+import cv2
+import PIL.Image as Image
+import numpy as np
+import h5py as h5
+import matplotlib.pyplot as plt
+
+
+
