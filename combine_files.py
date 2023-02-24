99c1de793c2f961e1388fc3caf628d54c686f407
Rado <radoslav.yankov96@gmail.com>
2023-02-24 10:24:51 +0100

    improve data pipeline

--- combine_files.py
+++ combine_files.py
@@ -6,10 +6,12 @@ from pathlib import Path
 data_path = Path("C:\\Users\\rados\\Desktop\\studies\\thesis\\code\\DatasetConstruction\\"
                  "some_data")
 
+
 for sequence in os.listdir(data_path):
     experiment_path = os.path.join(data_path, sequence)
-    os.chdir(experiment_path)
+    os.chdir(f"{data_path}\\dataset")
     with h5py.File(f"{sequence}.h5", mode='w') as h5fw:
+        os.chdir(experiment_path)
         for i, h5name in enumerate(glob.glob('01022023*.h5')):
             h5old = h5py.File(h5name, 'r')
             data = h5old["D2_dataNorm"]
