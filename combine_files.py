77d3bb832edd3b719c49bb92c1cf3afadddf5882
RadoslavYankov96 <radoslav.yankov96@gmail.com>
2023-03-29 14:31:45 +0200

    fix not sorted mistake

--- combine_files.py
+++ combine_files.py
@@ -7,11 +7,13 @@ data_path = Path("/home/itsnas/ueuua/BA/extra_images/")
 
 
 for sequence in os.listdir(data_path):
+    print(sequence)
     experiment_path = os.path.join(data_path, sequence)
     os.chdir(f"{data_path}/dataset")
     with h5py.File(f"{sequence}.h5", mode='w') as h5fw:
         os.chdir(experiment_path)
-        for i, h5name in enumerate(glob.glob('01022023*.h5')):
+        for i, h5name in enumerate(sorted(glob.glob('01022023*.h5'))):
+            print(h5name)
             h5old = h5py.File(h5name, 'r')
             data = h5old["D2_dataNorm"]
             h5fw.create_dataset("frame " + str(i + 1), data=data)
